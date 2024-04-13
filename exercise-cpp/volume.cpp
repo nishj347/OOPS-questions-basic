2c50cc9cfc64ba89d45ac437e928bb4fa39408d8
nish <144939153+nishj347@users.noreply.github.com>
2024-04-14 00:32:32 +0530

    question7

--- exercise-cpp/volume.cpp
+++ exercise-cpp/volume.cpp
@@ -2,7 +2,6 @@
 //Write a program to print the volume of a box by creating a class named 'Volume'
 //with an initialization list to initialize its length, breadth and height. (just to make you
 //familiar with initialization lists)
-
 #include <iostream>
 using namespace std;
 
